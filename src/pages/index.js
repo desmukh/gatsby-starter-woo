f59f9b79a7879a1d0a0eadb655deef5ba463bb1d
desmukh <desmukh@users.noreply.github.com>
2020-04-30 02:16:55 +0500

    Copied images and styles from Woo template

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
 
+import "../styles/default.css"
+import "../styles/layout.css"
+import "../styles/media-queries.css"
+import "../styles/animate.css"
+import "../styles/prettyPhoto.css"
+
 export default () => <div>Hello world!</div>
