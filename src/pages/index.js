0340a04d2ca2543c08b39e9be820f201fa15ce81
desmukh <desmukh@users.noreply.github.com>
2020-04-30 02:31:32 +0500

    Installed Helmet plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 
 import "../styles/default.css"
 import "../styles/layout.css"
@@ -6,4 +7,16 @@ import "../styles/media-queries.css"
 import "../styles/animate.css"
 import "../styles/prettyPhoto.css"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <>
+    <Helmet>
+      <title>Corona Solidarity Campaign</title>
+      <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+    </Helmet>
+    <header></header>
+    <main>
+      <div>Hello world!</div>
+    </main>
+  </>
+
+)
