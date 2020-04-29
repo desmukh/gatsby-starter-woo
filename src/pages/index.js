300342ddf2fcd9617e04cd508f455361d577e94b
desmukh <desmukh@users.noreply.github.com>
2020-04-30 03:26:28 +0500

    Added features section

--- src/pages/index.js
+++ src/pages/index.js
@@ -62,7 +62,73 @@ export default () => (
         </div>
       </section>
 
-    </main>
+      <section id='features'>
+
+        <div class="row feature design">
+         <div class="six columns right">
+           <h3>Simple, Clean and Modern Design.</h3>
+
+           <p>Lorem ipsum dolor sit amet, ea eum labitur scripserit, illum complectitur deterruisset at pro. Odio quaeque reformidans est eu, expetendis intellegebat has ut, viderer invenire ut his. Has molestie percipit an. Falli volumus efficiantur sed id, ad vel noster propriae. Ius ut etiam vivendo, graeci iudicabit constituto at mea. No soleat fabulas prodesset vel, ut quo solum dicunt. Nec et amet vidisse mentitum. Cibo mutat nulla ei eam.
+           </p>
+         </div>
+
+         <div class="six columns feature-media left">
+           <img src="images/feature-image-1.png" alt="" />
+         </div>
+
+       </div>
+
+       <div class="row feature responsive">
+
+         <div class="six columns left">
+           <h3>Fully Responsive.</h3>
+
+           <p>Aenean condimentum, lacus sit amet luctus lobortis, dolores et quas molestias excepturi enim tellus ultrices elit, amet consequat enim elit noneas sit amet luctu. Quis nostrum exercitationem ullam corporis suscipit laboriosam.Our library is continually refreshed with the latest on web technology so you'll never fall behind. Quis nostrum exercitationem ullam corporis suscipit laboriosam.
+           </p>
+         </div>
+
+         <div class="six columns feature-media right">
+           <img src="images/feature-image-2.png" alt="" />
+         </div>
+
+       </div>
+
+       <div class="row feature cross-browser">
+
+         <div class="six columns right">
+           <h3>Cross-Browser Compatible.</h3>
+
+           <p>Lorem ipsum dolor sit amet, ea eum labitur scripserit, illum complectitur deterruisset at pro. Odio quaeque reformidans est eu, expetendis intellegebat has ut, viderer invenire ut his. Has molestie percipit an. Falli volumus efficiantur sed id, ad vel noster propriae. Ius ut etiam vivendo, graeci iudicabit constituto at mea. No soleat fabulas prodesset vel, ut quo solum dicunt. Nec et amet vidisse mentitum. Cibo mutat nulla ei eam.
+           </p>
+         </div>
+
+         <div class="six columns feature-media left">
+           <img src="images/feature-image-3.png" alt="" />
+         </div>
+
+       </div>
+
+       <div class="row feature video">
+
+         <div class="six columns left">
+           <h3>Video Support.</h3>
+
+           <p>Aenean condimentum, lacus sit amet luctus lobortis, dolores et quas molestias excepturi enim tellus ultrices elit, amet consequat enim elit noneas sit amet luctu. Quis nostrum exercitationem ullam corporis suscipit laboriosam. No soleat fabulas prodesset vel, ut quo solum dicunt. Nec et amet vidisse mentitum. Cibo mutat nulla ei eam.
+           </p>
+         </div>
+
+         <div class="six columns feature-media right">
+           <div class="fluid-video-wrapper">
+             <iframe title="video" src="http://player.vimeo.com/video/14592941?title=0&amp;byline=0&amp;portrait=0&amp;color=F64B39" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen>
+             </iframe>
+           </div>
+         </div>
+
+       </div>
+
+     </section>
+
+   </main>
   </>
 
 )
