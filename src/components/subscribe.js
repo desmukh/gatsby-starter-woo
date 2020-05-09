27fe7889a971a0e9e3ecaff344a33b9d8691f55e
desmukh <desmukh@users.noreply.github.com>
2020-05-10 04:24:38 +0500

    changed value key to defaultValue on submission form

--- src/components/subscribe.js
+++ src/components/subscribe.js
@@ -18,11 +18,11 @@ export default () => (
       <div id="mc_embed_signup">
         <form action="http://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e65110b38d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
           <ScrollAnimation animateIn="fadeInLeftBig" animateOnce={true}>
-          <input type="email" value="" name="EMAIL" className="email" id="mce-EMAIL" placeholder="email address" required /  >
+          <input type="email" defaultValue="" name="EMAIL" className="email" id="mce-EMAIL" placeholder="email address" required /  >
           </ScrollAnimation>
              {/* <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups--> */}
-             <div style={{position: "absolute", left: "-5000px"}}><input type="text" name="b_cdb7b577e41181934ed6a6a44_e65110b38d" value="" /></div>
-             <div className="clear"><ScrollAnimation animateIn="fadeInRightBig" animateOnce={true}><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button" /></ScrollAnimation></div>
+             <div style={{position: "absolute", left: "-5000px"}}><input type="text" name="b_cdb7b577e41181934ed6a6a44_e65110b38d" defaultValue="" /></div>
+             <div className="clear"><ScrollAnimation animateIn="fadeInRightBig" animateOnce={true}><input type="submit" defaultValue="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button" /></ScrollAnimation></div>
 
            </form>
          </div>
