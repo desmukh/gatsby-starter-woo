0edf97ff1cab54514300d8116e013fb768f9ee27
desmukh <desmukh@users.noreply.github.com>
2020-05-28 04:13:41 +0500

    Moved screenshots content to yaml

--- src/components/screenshots.js
+++ src/components/screenshots.js
@@ -2,16 +2,9 @@ import React, { Component } from 'react';
 import Lightbox from 'react-image-lightbox';
 import 'react-image-lightbox/style.css'; // This only needs to be imported once in your app
 
-const images = [
-  {big:'b-line-icons.jpg', thumb:'line-icons.jpg', caption:'Line Icons' },
-  {big:'b-hipster.jpg', thumb:'hipster.jpg', caption:'Hipster' },
-  {big:'b-authentic-vintage.jpg', thumb:'authentic-vintage.jpg', caption:'Authentic Vintage'},
-  {big:'b-spot-uv-logo.jpg', thumb:'spot-uv-logo.jpg', caption:'Spot UV Logo'},
-  {big:'b-tshirt-mockup.jpg', thumb:'tshirt-mockup.jpg', caption:'TShirt Mockup'},
-  {big:'b-abstract-vector.jpg', thumb:'abstract-vector.jpg', caption:'Abstract Vector'},
-  {big:'b-embossed-paper.jpg', thumb:'embossed-paper.jpg', caption:'Embossed Paper'},
-  {big:'b-judah.jpg', thumb:'judah.jpg', caption:'Judah'}
-];
+import content from '../../content/screenshots.yaml'
+
+const screenshots = content.screenshots
 
 export default class LightboxExample extends Component {
   constructor(props) {
@@ -30,26 +23,31 @@ export default class LightboxExample extends Component {
       <section id="screenshots">
 
         <div className="row section-head">
-          <h1>Product Screenshots.</h1>
-          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.
-       </p>
+          <h1>{ content.title }</h1>
+          <p>{ content.body }</p>
      </div>
      <div className="row">
        <div className="twelve columns">
          <div id="screenshots-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
 
-           {images.map((value, index) => {
+           {screenshots.map((screenshot, index) => {
              return (
                <div className="columns item" key={index.toString()}>
                  <div className="item-wrap">
 
-                   <a href={'images/screenshots/big/' + images[index].big} data-imagelightbox="a" onClick={event => {
+                   <a
+                     href={'images/screenshots/big/' + screenshot.big}
+                     data-imagelightbox="a" onClick={event => {
                         event.preventDefault()
                         this.setState({ isOpen: true, photoIndex: index, })
-                    }}>
-                     <img src={'images/screenshots/' + images[index].thumb} alt={images[index].caption} />
+                  }}>
+                     <img
+                       src={'images/screenshots/' + screenshot.thumb}
+                       alt={screenshot.caption} />
                        <div className="overlay"></div>
-                         <div className="link-icon"><i className="icon-plus"></i></div>
+                         <div className="link-icon">
+                           <i className="icon-plus"></i>
+                         </div>
                          </a>
                      </div>
                    </div>
@@ -61,26 +59,31 @@ export default class LightboxExample extends Component {
          </div>
       <div>
 
-        {isOpen && (
-          <Lightbox
-            mainSrc={'images/screenshots/big/' + images[photoIndex].big}
-            nextSrc={'images/screenshots/big/' + images[(photoIndex + 1) % images.length].big}
-            prevSrc={'images/screenshots/big/' + images[(photoIndex + images.length - 1) % images.length].big}
-            onCloseRequest={() => this.setState({ isOpen: false })}
-            onMovePrevRequest={() =>
-              this.setState({
-                photoIndex: (photoIndex + images.length - 1) % images.length,
-              })
-            }
-            onMoveNextRequest={() =>
-              this.setState({
-                photoIndex: (photoIndex + 1) % images.length,
-              })
-            }
-            imageCaption={images[photoIndex].caption}
-            enableZoom={false}
-          />
-        )}
+      {isOpen && (
+        <Lightbox
+          mainSrc={
+            'images/screenshots/big/' + screenshots[photoIndex].big
+          }
+          nextSrc={
+            'images/screenshots/big/' + screenshots[(photoIndex + 1) % screenshots.length].big
+          }
+          prevSrc={
+            'images/screenshots/big/' + screenshots[(photoIndex + screenshots.length - 1) % screenshots.length].big}
+          onCloseRequest={() => this.setState({ isOpen: false })}
+          onMovePrevRequest={() =>
+            this.setState({
+              photoIndex: (photoIndex + screenshots.length - 1) % screenshots.length,
+            })
+          }
+          onMoveNextRequest={() =>
+            this.setState({
+              photoIndex: (photoIndex + 1) % screenshots.length,
+            })
+          }
+          imageCaption={screenshots[photoIndex].caption}
+          enableZoom={false}
+        />
+      )}
       </div></section>
     );
   }
