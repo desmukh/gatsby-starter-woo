065566cfcc5391d407f8c1500f28337485c84aa3
desmukh <desmukh@users.noreply.github.com>
2020-05-28 04:29:36 +0500

    Moved testimonials content to yaml

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -1,7 +1,10 @@
 import React from "react"
 import Slider from "react-slick"
 
+import content from '../../content/testimonials.yaml'
+
 const MySlider = () =>{
+
   const settings = {
     dots: true,
     infinite: true,
@@ -13,37 +16,31 @@ const MySlider = () =>{
     autoplay: true,
     autoplaySpeed: 7000,
     draggable: false,
-
   }
+
   return (
     <section id="testimonials">
       <div className="row content">
         <span><i className="quote-left fa fa-quote-left"></i></span>
         <div className="text-container">
 
-      <div>
-        <h1>What People Are Saying.</h1>
-        <Slider {...settings} style={{ userSelect: 'text' }}>
           <div>
-            <blockquote>
-              <p>Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do. If you haven't found it yet, keep looking. Don't settle. As with all matters of the heart, you'll know when you find it.
-              </p>
-              <cite>Steve Jobs</cite>
-            </blockquote>
+            <h1>{ content.title }</h1>
+            <Slider {...settings} style={{ userSelect: 'text' }}>
+              { content.quotes.map((quote, index) => (
+                <div>
+                  <blockquote>
+                    <p>{ quote.text }</p>
+                    <cite>{ quote.cite }</cite>
+                  </blockquote>
+                </div>
+              ))}
+            </Slider>
           </div>
-          <div>
-            <blockquote>
-              <p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris.
-              </p>
-              <cite>Mr. Adobe</cite>
-            </blockquote>
-          </div>
-        </Slider>
-      </div>
-    </div>
-    <span><i className="quote-right fa fa-quote-right"></i></span>
 
-    </div>
+        </div>
+        <span><i className="quote-right fa fa-quote-right"></i></span>
+      </div>
     </section>
   )
 }
