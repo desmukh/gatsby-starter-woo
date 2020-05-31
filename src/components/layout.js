a6c9edb4d770519df0017cbae01cc4bc8a2943d0
desmukh <desmukh@users.noreply.github.com>
2020-05-31 20:51:09 +0500

    Testimonial quotes now use react-fontawesome. Fixed dots styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -26,8 +26,7 @@ export default function Layout ({ children }){
     	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 
       <link rel="shortcut icon" href={'/' + favicon } type="image/png" />
-      <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
-      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+
     </Helmet>
 
     <Header />
--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -1,7 +1,10 @@
 import React from "react"
 import Slider from "react-slick"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faQuoteRight, faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
 
 import content from '../../content/testimonials.yaml'
+
 
 const MySlider = () =>{
 
@@ -16,30 +19,39 @@ const MySlider = () =>{
     autoplay: true,
     autoplaySpeed: 7000,
     draggable: false,
+    appendDots: dots => (
+      <ol> {dots} </ol>
+    ),
+    customPaging: i => (
+      <button>
+        {i + 1}
+      </button>
+    ),
   }
 
   return (
     <section id="testimonials">
       <div className="row content">
-        <span><i className="quote-left fa fa-quote-left"></i></span>
+        <span><i className="quote-left fa"><FontAwesomeIcon icon={ faQuoteLeft } /></i></span>
         <div className="text-container">
-
-          <div>
+          <div className="twelve columns">
             <h1>{ content.title }</h1>
-            <Slider {...settings} style={{ userSelect: 'text' }}>
-              { content.quotes.map((quote, index) => (
-                <div>
-                  <blockquote>
-                    <p>{ quote.text }</p>
-                    <cite>{ quote.cite }</cite>
-                  </blockquote>
-                </div>
-              ))}
-            </Slider>
+          </div>
+          <div class="twelve columns flex-container">
+          <Slider {...settings} style={{ userSelect: 'text' }}>
+            { content.quotes.map((quote, index) => (
+              <div>
+                <blockquote>
+                  <p>{ quote.text }</p>
+                  <cite>{ quote.cite }</cite>
+                </blockquote>
+              </div>
+            ))}
+          </Slider>
           </div>
 
         </div>
-        <span><i className="quote-right fa fa-quote-right"></i></span>
+        <span><i className="quote-right fa"><FontAwesomeIcon icon={ faQuoteRight } /></i></span>
       </div>
     </section>
   )
